927301fadb67cd4865b8f17082f2a4d405c807a9
Wataru TOrii <eagle.hokkaido2317@gmail.com>
2012-10-16 11:11:59 +0900

    Functorのinstance化まで

--- Test1.hs
+++ Test1.hs
@@ -2,5 +2,21 @@ module Test1 where
 
 import Control.Monad.Trans.Free
 
+data Toy b next = 
+	  Output b next
+	| Bell next
+	| Done
+
+data FixE f e = Fix (f (FixE f e)) | Throw e
+
+instance Functor (Toy b) where
+	fmap f (Output x next) = Output x (f next)
+	fmap f (Bell     next) = Bell     (f next)
+	fmap f  Done           = Done
+
+catchF :: (Functor f) => FixE f e1 -> (e1 -> FixE f e2) -> FixE f e2
+catchF (Fix x) f = Fix (fmap (flip catchF f) x)
+catchF (Throw e) f = f e
+
 run :: Free f r -> FreeF f r (Free f r)
 run m = runFree m
